de66cfd9f2111cacda87d226df29b1853e6d5a38
Felix Khoja <o.khoja93@gmail.com>
2021-04-04 05:32:44 -0400

    Add Travis CI yml config

--- QuizEngine/Flow.swift
+++ QuizEngine/Flow.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class Flow <Question, Answer, R: Router> where R.Question == Question, R.Answer == Answer {
-    private let router:  R
+    private let router: R
     private let questions: [Question]
     private var answers: [Question:Answer] = [:]
     private var scoring: ([Question:Answer]) -> Int
